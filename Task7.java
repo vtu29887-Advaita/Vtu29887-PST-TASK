9fe85da3ca82bf0aaa2a4311a1938b84fae7852f
Advaita <vtu29887@veltech.edu.in>
2026-07-31 01:14:00 +0530

    Addded to folder

---
+++ Task7.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int evenSum = 0;
+        int oddSum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (digit % 2 == 0)
+                evenSum += digit;
+            else
+                oddSum += digit;
+
+            num /= 10;
+        }
+
+        System.out.println("Sum of even digits = " + evenSum);
+        System.out.println("Sum of odd digits = " + oddSum);
+
+        sc.close();
+    }
+}
