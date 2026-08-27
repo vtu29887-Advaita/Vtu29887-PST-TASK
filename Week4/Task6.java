3375e46427bb90cb6faa66bedbdd5d5597d6f2ea
Advaita <vtu29887@veltech.edu.in>
2026-08-27 09:15:21 +0530

    added to folder

--- Week4/Task6.java
+++ Week4/Task6.java
@@ -13,8 +13,17 @@ public class Task6 {
         }
     }
     public static void main(string[]args){
-        int[] nums={0,1,0,3,12};
-        moveZeros(nums);
-        system.out.println(Arrays.toString(nums));
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the size of the array");
+    int n=sc.nextInt();
+    int[]nums=new int[n];
+    System.out.println("Enter the elements of the array");
+    for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+    movezeros(nums);
+    for(int i=0;i<n;i++){
+            System.out.print("output: "+ Arrays.toString(nums));
+            sc.close();
+        }
     }
-}
